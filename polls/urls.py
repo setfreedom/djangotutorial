4c8cb94e9b7c81ba139b7cd4aa824bc0156ebe34
cdx12138 <2513724121@qq.com>
2025-12-11 21:05:34 +0800

    2025年12月11日 第二次

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
 from . import views
-
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    # 例如：/polls/5/
+    path("<int:question_id>/", views.detail, name = "detail"),
+    # 例如： /polls/5/results/
+    path("<int:question_id>/results/", views.results, name = "results"),
+    # 例如： /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name = "vote"),
 ]
